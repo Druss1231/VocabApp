c951321fd6f878ff62b4e64cacce9fa12f95b17d
Druss1231 <drussyryr@gmai.com>
2025-08-02 23:33:44 +0900

    ui: fixed max width (test)

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+    server: {
+    host: true, // ←これを追加
+    port: 5174, // ←任意のポート番号（デフォルト5173）
+  },
 })
